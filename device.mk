88299b93b30bd1ee0c46e30461d36bf937fe780e
suchit-7x <gtsuchit7x@gmail.com>
2026-09-19 20:03:25 +0530

    honda: Import minimalized init from CPH2719_16.0.5.1002(EX01)

--- device.mk
+++ device.mk
@@ -61,6 +61,26 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
     android.hardware.health-service.mediatek-recovery
 
+# Init
+PRODUCT_PACKAGES += \
+    fstab.mt6897 \
+    fstab.mt6897.vendor_ramdisk \
+    init_conninfra.rc \
+    init.aee.rc \
+    init.cgroup.rc \
+    init.connectivity.common.rc \
+    init.modem.rc \
+    init.mt6897.rc \
+    init.mt6897.usb.rc \
+    init.mtkgki.rc \
+    init.project.rc \
+    init.sensor_2_0.rc \
+    ueventd.mt6897.rc
+
+PRODUCT_PACKAGES += \
+    init.insmod.sh \
+    init.insmod.mt6897.cfg
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
